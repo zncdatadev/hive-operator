2add0fbcce72794ef61bdfdcc9a0e05d9ebfa9a8
whg517 <huagang517@126.com>
2025-12-21 18:43:46 +0800

    refactor: reorganize test setup and enhance HiveMetastore controller tests

--- internal/controller/metastore_controller_test.go
+++ internal/controller/metastore_controller_test.go
@@ -5,42 +5,74 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	hivev1alpha1 "github.com/zncdatadev/hive-operator/api/v1alpha1"
+
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	hivev1alpha1 "github.com/zncdatadev/hive-operator/api/v1alpha1"
 )
 
 var _ = Describe("HiveMetastore controller", func() {
 
-	Context("Reconcile", func() {
-		It("Should reconcile a HiveMetastore resource", func() {
-			ctx := context.Background()
+	Context("When reconciling a resource", func() {
+		const resourceName = "test-hivemetastore"
 
-			// Create a HiveMetastore resource
-			hiveMetastore := &hivev1alpha1.HiveMetastore{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "test-hivemetastore",
-					Namespace: "default",
-				},
-				Spec: hivev1alpha1.HiveMetastoreSpec{
-					ClusterConfig: &hivev1alpha1.ClusterConfigSpec{
-						Database: &hivev1alpha1.DatabaseSpec{},
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name:      resourceName,
+			Namespace: "default",
+		}
+		hiveMetastore := &hivev1alpha1.HiveMetastore{}
+
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind HiveMetastore")
+			err := k8sClient.Get(ctx, typeNamespacedName, hiveMetastore)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &hivev1alpha1.HiveMetastore{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceName,
+						Namespace: "default",
 					},
-					Metastore: &hivev1alpha1.RoleSpec{
-						RoleGroups: map[string]*hivev1alpha1.RoleGroupSpec{
-							"default": {
-								Replicas: 1,
+					Spec: hivev1alpha1.HiveMetastoreSpec{
+						ClusterConfig: &hivev1alpha1.ClusterConfigSpec{
+							Database: &hivev1alpha1.DatabaseSpec{},
+						},
+						Metastore: &hivev1alpha1.RoleSpec{
+							RoleGroups: map[string]*hivev1alpha1.RoleGroupSpec{
+								"default": {
+									Replicas: 1,
+								},
 							},
 						},
 					},
-				},
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
 			}
-			Expect(k8sClient.Create(ctx, hiveMetastore)).To(Succeed())
+		})
 
-			// List HiveMetastore resources and verify the created resource is present
-			objs := &hivev1alpha1.HiveMetastoreList{}
-			Expect(k8sClient.List(ctx, objs)).To(Succeed())
-			Expect(objs.Items).To(HaveLen(1))
-			Expect(objs.Items[0].Name).To(Equal("test-hivemetastore"))
+		AfterEach(func() {
+			resource := &hivev1alpha1.HiveMetastore{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance HiveMetastore")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+		})
+
+		It("should successfully reconcile the resource", func() {
+			By("Reconciling the created resource")
+			controllerReconciler := &HiveMetastoreReconciler{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
 		})
 	})
 })
